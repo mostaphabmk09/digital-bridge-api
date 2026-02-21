2d1c596a6b48c1ced090f828dc31894e7fdd525c
mustaphabaimik <mustaphabaimik@gmail.com>
2026-02-21 16:57:55 +0100

    feat: :wrench: cors config

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+   app.enableCors({
+    origin: 'http://localhost:3000', // front URL
+    credentials: true, // for cookies
+  });
+
   app.use(cookieParser());
   
   app.useGlobalPipes(
